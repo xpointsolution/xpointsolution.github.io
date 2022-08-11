d6b7bb52541698336a8ccb36d117dddf23959835
Alexander Mudzo <alexander.mudzo@gmail.com>
2022-08-11 19:16:11 +0200

    Updates

---
+++ precache-manifest.d71454bc2aca33828d72622cf46a7794.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f190b40757f74dca1eb19db9b00392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb6b781bc4159c455f6",
+    "url": "/static/css/2.4457f4c4.chunk.css"
+  },
+  {
+    "revision": "073cad2d35e217f225a2",
+    "url": "/static/css/main.d0b276d2.chunk.css"
+  },
+  {
+    "revision": "9fb6b781bc4159c455f6",
+    "url": "/static/js/2.781aca4e.chunk.js"
+  },
+  {
+    "revision": "3abf7c0bd304e73af176",
+    "url": "/static/js/3.021acb2a.chunk.js"
+  },
+  {
+    "revision": "073cad2d35e217f225a2",
+    "url": "/static/js/main.904e9858.chunk.js"
+  },
+  {
+    "revision": "254ea5ea4c442741514f",
+    "url": "/static/js/runtime~main.a269b07f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
